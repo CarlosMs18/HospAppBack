7cc7e8dc0f848f46e3eeec42f9d794e0c682c293
carlos <carlos.melgarejo.solorzano@hotmail.com>
2022-11-11 10:03:18 -0500

    commit

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,7 @@ const {Router} = require('express')
 const { check } = require('express-validator')
 const { getUsuario, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios')
 const { validarCampos } = require('../middlewares/validar-campo')
-const { validarJWT } = require('../middlewares/validar-jwt')
+const { validarJWT, validarADMIN_ROLE, validarADMIN_ROLE_o_mismo_usuario } = require('../middlewares/validar-jwt')
 
 const router = Router()
 
@@ -21,6 +21,7 @@ router.post('/',[
 
 router.put('/:id',
     validarJWT,    
+    validarADMIN_ROLE_o_mismo_usuario,
     [
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     /* check('password','El password es obligatorio').not().isEmpty(), */
@@ -31,6 +32,6 @@ router.put('/:id',
 ,actualizarUsuario)
 
 
-router.delete('/:id',validarJWT ,borrarUsuario)
+router.delete('/:id',[validarJWT , validarADMIN_ROLE] ,borrarUsuario)
 
 module.exports = router
